fbed28117f28c11b8ac83d8d484a4ca1541a446c
Ilya Pirogov <ilya-pirogov@users.noreply.github.com>
2016-01-27 21:21:02 -0800

    fixed symlinks

--- server/setup-env.sh
+++ server/setup-env.sh
@@ -10,10 +10,10 @@ echo eula=true > eula.txt
 
 if [[ ! -f ./$FORGE_UNIVERSAL ]]; then
     mkdir settings
-    ln -s banned-ips.json settings/banned-ips.json
-    ln -s banned-players.json settings/banned-players.json
-    ln -s ops.json settings/ops.json
-    ln -s whitelist.json settings/whitelist.json
+    ln -s settings/banned-ips.json banned-ips.json
+    ln -s settings/banned-players.json banned-players.json
+    ln -s settings/ops.json ops.json
+    ln -s settings/whitelist.json whitelist.json
 
     wget http://files.minecraftforge.net/maven/net/minecraftforge/forge/$VERSION/$FORGE_INSTALLER
     echo $FORGE_INSTALLER_SHA1 $FORGE_INSTALLER | sha1sum -c
