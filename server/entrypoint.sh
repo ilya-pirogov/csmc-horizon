09d10bc7caf3cb27608bb6be0318e187c8c4c63c
Ilya Pirogov <ilya-pirogov@users.noreply.github.com>
2016-05-12 01:33:56 -0700

    Update entrypoint.sh

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -4,7 +4,7 @@ VERSION=$MINECRAFT_VERSION-$FORGE_VERSION-$MINECRAFT_VERSION
 FORGE_UNIVERSAL=forge-$VERSION-universal.jar
 
 if [[ ! -z $JMX_HOSTNAME ]]; then
-    JMX_ARGS="-Djava.rmi.server.hostname='$JMX_HOSTNAME' -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=$JMX_PORT -Dcom.sun.management.jmxremote.rmi.port=$JMX_PORT -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.local.only=false"
+    JMX_ARGS="-Djava.rmi.server.hostname='$JMX_HOSTNAME' -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=3333 -Dcom.sun.management.jmxremote.rmi.port=3334 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.local.only=false"
 fi
 
 if [[ ! -z $RCON_PASSWORD ]]; then
@@ -29,4 +29,4 @@ sed -ir "s/rcon\.password=.*/rcon.password=$RCON_PASSWORD/g" server.properties
 sed -ir "s/S:channel=.*/S:channel=$TWITCH_CHANNEL/g" config/ForgeTwitchSubWhitelist.cfg
 sed -ir "s/S:twitchToken=.*/S:twitchToken=$TWITCH_TOKEN/g" config/ForgeTwitchSubWhitelist.cfg
 
-java -jar $JMX_ARGS $JVM_ADDITIONAL -Xms$JVM_XMS -Xmx$JVM_XMX $FORGE_UNIVERSAL nogui
+java $JMX_ARGS $JVM_ADDITIONAL -Xms$JVM_XMS -Xmx$JVM_XMX -jar $FORGE_UNIVERSAL nogui
